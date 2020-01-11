3f9c8ad7094efe64df98e7c96ff05bd46737f203
Seong-Kyu Yun <reviday@naver.com>
2020-01-11 17:52:15 +0900

    #5 Updating the Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,19 +3,28 @@ import AppPresenter from "./AppPresenter";
 import Store from "store"; // env에 설정한 값 떄문에 store만 해도 호출 가능 => node environment
 
 class AppContainer extends Component {
-  state = {
-    message : "Hello"
+  constructor(props) {
+    super(props);
+    this._changeMessage = () => {
+      if(this.state.message === "Hello") {
+        this.setState({
+          message: "Bye bye"
+        })
+      } else {
+        this.setState({
+          message: "Hello" 
+        })
+      }
+    };
+    this.state= {
+      message : "Hello",
+      changeMessage : this._changeMessage
+    }
   }
-  componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({
-        message: "Bye"
-      })
-    }, 2000);
-  }
+  
   render() {
     return (
-      <Store.Provider value={this.state.message}>
+      <Store.Provider value={this.state}>
         <AppPresenter />
       </Store.Provider>
     );
@@ -23,3 +32,8 @@ class AppContainer extends Component {
 }
 
 export default AppContainer;
+
+/*
+  #state를 변경하는 함수를 만들고 싶다. 그리고 그걸 provider 안에 넣고 싶음.(notification에서 부를 수 있도록)
+  => 모든 것을 constructor 안에 넣어야 한다. 왜냐하면 클래스가 생성되었을때 스토어가 value를 얻기 때문
+*/
