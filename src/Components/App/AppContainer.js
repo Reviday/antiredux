1ee186553cc6fcac468002235044d0d9373023e4
Seong-Kyu Yun <reviday@naver.com>
2020-01-11 17:31:32 +0900

    #4 Consuming the Store

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,9 +3,19 @@ import AppPresenter from "./AppPresenter";
 import Store from "store"; // env에 설정한 값 떄문에 store만 해도 호출 가능 => node environment
 
 class AppContainer extends Component {
+  state = {
+    message : "Hello"
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        message: "Bye"
+      })
+    }, 2000);
+  }
   render() {
     return (
-      <Store.Provider>
+      <Store.Provider value={this.state.message}>
         <AppPresenter />
       </Store.Provider>
     );
